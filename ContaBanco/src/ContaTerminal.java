8e5f93e832bc005e4cc1797f291f8e2c502e08e3
Mayra Tebaldi <mayratebaldi@hotmail.com>
2024-09-05 18:24:10 -0300

    Iniciando o projeto

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //Conhecer e importar a classe Sacanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem de conta criada
+    }
+}
